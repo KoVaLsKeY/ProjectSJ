94b4cbaed55b3654030251274cab827b0da23366
KoVaLsKeY <vladyslav.kovalov@student.ukf.sk>
2025-03-04 16:13:49 +0100

    Added code
    I've added some code

--- index.php
+++ index.php
@@ -1,3 +1,78 @@
+
+<!DOCTYPE
+html
+>
+<html
+lang
+="
+sk
+"
+>
+< head >
+<
+meta
+charset
+="UTF
+-
+8"
+>
+<
+meta
+name
+="
+viewport
+"
+content
+="
+width
+=device
+-
+width
+, 
+initial
+-
+scale
+=1.0"
+>
+<title>
+Moja prvá stránka v PHP
+</title>
+</
+head
+>
+<body>
+<h1>
+Vitajte na mojej prvej stránke v PHP!
+</h1>
 <?php
-echo"Hello World";
+echo
+"<h1>
+Henlo
+world
+</h1>"
+;
+echo
+"Dnes je "
+.
+date
+(
+"
+d.m.Y
+"
+) .
+"<br>"
+;
+echo
+"Aktuálny čas je "
+.
+date
+(
+"
+H:i:s
+"
+) .
+"<br>"
+;
 ?>
+</body>
+</html>
