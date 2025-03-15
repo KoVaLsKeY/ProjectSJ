66e6092b770f50cae9b7c577ca2b6b7b35c7030f
Vladyslav Kovalov <vladkovalskey@gmail.com>
2025-03-15 18:15:31 +0100

    changed something

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
-<html lang="sk">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moja prvá stránka v PHP</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Vitajte na mojej prvej stránke v PHP!</h1>
     <?php
-        echo "<h1>Henlo world</h1>";
-        echo "Dnes je " .date("d.m.Y") . "<br>";
-        echo "Aktuálny čas je " .date("H:i:s") . "<br>";
+    echo "Hello world!";
     ?>
 </body>
 </html>
